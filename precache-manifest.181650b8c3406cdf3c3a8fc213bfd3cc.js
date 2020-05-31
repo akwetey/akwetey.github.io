544a4b2806cf4e481f116f6040b6e951cd6785fb
Jonathan Akwetey <okinejonathan@yahoo.com>
2020-05-31 19:31:39 +0100

    create react app portfolio site

---
+++ precache-manifest.181650b8c3406cdf3c3a8fc213bfd3cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d433e42e5ff2671c87fe0448e93ce8f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44e21560417f63891f09",
+    "url": "/static/css/main.de5bd79f.chunk.css"
+  },
+  {
+    "revision": "c7b673d7c7e82eaf6751",
+    "url": "/static/js/2.650d14ea.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.650d14ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44e21560417f63891f09",
+    "url": "/static/js/main.3c77a4d2.chunk.js"
+  },
+  {
+    "revision": "5284eec07972ace79c95",
+    "url": "/static/js/runtime-main.ceaf4b5f.js"
+  }
+]);
